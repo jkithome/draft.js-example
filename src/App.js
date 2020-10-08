567decab93c299f762d09d76454d22f5370ce261
Jeremy Kithome <jerekith@gmail.com>
2020-10-08 15:37:19 +0300

    feat: display editor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Rich Text Editor Example
       </header>
+      <Editor editorState={editorState} onChange={setEditorState} />
     </div>
-  );
+  )
 }
 
 export default App;
